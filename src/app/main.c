b5c9c48a8ef04c0d8c48ffcf50048327c63c5c3b
Isak Simonsson <isak.simonsson@gmail.com>
2026-01-06 10:39:35 +0100

    remove unnecessary refresh and group initial draw in main

--- src/app/main.c
+++ src/app/main.c
@@ -28,17 +28,14 @@ int main(void) {
     UIPanelCurses main_menu = menu_main_create(max_x);
     init_color_scheme(main_menu.win, TRON_ORANGE);
 
-    ui_panel_curses_draw(&main_menu);
-
     int ta_y = main_menu.panel->element_count + UI_BORDER_PADDING * 2;
     UIPanelCurses ta = test_area_create(max_x, ta_y, max_y);
-
     init_color_scheme(ta.win, TRON_ORANGE);
 
     RenderContext ta_ctx = render_context_new(ta.win);
 
+    ui_panel_curses_draw(&main_menu);
     ui_panel_curses_draw(&ta);
-    wrefresh(ta.win);
 
     // iterates over all chars in the text, and creates draw commands for all of them.
     TypingTest tt = typing_test_new_english("this is an example typing test lorem ipsum dolor sit amet");
